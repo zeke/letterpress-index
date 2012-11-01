c341071f1562c009254574bcf84670ef1cb61f16
zeke <zeke@sikelianos.com>
2012-11-01 15:30:37 -0700

    moar facts

--- compare.rb
+++ compare.rb
@@ -1,4 +1,4 @@
-sowpods = File.readlines("sowpods.txt").map(&:chomp).map(&:downcase)
-letterpress = File.readlines("letterpress_alphabetical.txt").map(&:chomp).map(&:downcase)
+sowpods = File.readlines("sowpods_alphabetical.txt").map(&:chomp)
+letterpress = File.readlines("letterpress_alphabetical.txt").map(&:chomp)
 File.open("letterpress_minus_sowpods.txt", 'w') {|f| f.write (letterpress-sowpods).join("\n") }
 File.open("sowpods_minus_letterpress.txt", 'w') {|f| f.write (sowpods-letterpress).join("\n") }
